5cf6e7b65d02ff4e7b56c5f09a18f15176b41093
Colin Schatz <cschatz@mills.edu>
2021-10-21 10:02:21 -0700

    Set up tree nodes for universities in CA

--- src/Day16.java
+++ src/Day16.java
@@ -1,23 +1,60 @@
 import java.util.ArrayList;
 
+
 public class Day16 {
 	
-	private class TreeNode<E> {
+	private static class TreeNode<E> {
 		E element;
 		TreeNode<E> parent;
 		ArrayList< TreeNode<E> > children;
 		
 		// constructor
-		public TreeNode(E element, TreeNode<E> parent) {
+		public TreeNode(E element) {
 			this.element = element;
-			this.parent = parent;
 			this.children = new ArrayList< TreeNode<E> >();
+		}
+		
+		public void addChild(TreeNode<E> node) {
+			children.add(node);
+			node.parent = this; 
+			// meaning of "this": the current object being acted on
+			// "this" is set to the object on the left side
+			//  of the dot(.) when a method call happens
 		}
 	}
 	
-
 	public static void main(String[] args) {
+		TreeNode<String> root = new TreeNode<String>("Univerities in CA");
 		
+		TreeNode<String> pub = new TreeNode<String>("public");
+		TreeNode<String> priv = new TreeNode<String>("private");
+		root.addChild(pub);
+		root.addChild(priv);
+		
+		TreeNode<String> uc = new TreeNode<String>("UCs");
+		pub.addChild(uc);
+		TreeNode<String> irvine = new TreeNode<String>("Irvine");
+		TreeNode<String> la = new TreeNode<String>("LA");
+		TreeNode<String> berkeley = new TreeNode<String>("Berkeley");
+		uc.addChild(irvine);
+		uc.addChild(la);
+		uc.addChild(berkeley);
+		
+		TreeNode<String> csu = new TreeNode<String>("CSUs");
+		pub.addChild(csu);
+		
+		TreeNode<String> longbeach = new TreeNode<String>("Long Beach");
+		TreeNode<String> sj = new TreeNode<String>("SJSU");
+		TreeNode<String> sf = new TreeNode<String>("SF State");
+		csu.addChild(longbeach);
+		csu.addChild(sj);
+		csu.addChild(sf);
+		
+		TreeNode<String> small = new TreeNode<String>("small");
+		priv.addChild(small);
+		TreeNode<String> mills = new TreeNode<String>("Mills");
+		small.addChild(mills);
+	
 	}
 
 }
