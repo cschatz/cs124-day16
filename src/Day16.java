94816e1a694d10bf03c11ce740cedef095a5390d
Colin Schatz <cschatz@mills.edu>
2021-10-21 09:35:40 -0700

    Create internal Node class

--- src/Day16.java
+++ src/Day16.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
 
 public class Day16 {
+	
+	private class TreeNode<E> {
+		E element;
+		TreeNode<E> parent;
+		ArrayList< TreeNode<E> > children;
+		
+		// constructor
+		public TreeNode(E element, TreeNode<E> parent) {
+			this.element = element;
+			this.parent = parent;
+			this.children = new ArrayList< TreeNode<E> >();
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		
